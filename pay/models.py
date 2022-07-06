5b697d126938baf719e638623cfe5be89f739b76
jonghyunyang <jonghyun5646@naver.com>
2022-07-06 18:21:44 +0900

    양종현 - pay html view 부분 수정, DB 추가

--- pay/models.py
+++ pay/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from member.models import Member
+
 
 # Create your models here.
+class Shop(models.Model):
+    cname = models.TextField(primary_key=True)
+    img = models.TextField(default='')
+    innertrait = models.TextField()
+    fighttrait = models.TextField()
+    skill = models.TextField()
+    price=models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'shop'     # 생성할 테이블 이름 지정
+        ordering = ['cname']
+
+    def __str__(self):
+        return self.cname     # 객체 출력시 출력형태 정의
+
+
+class Possession(models.Model):
+    id = models.AutoField(primary_key=True)
+    userid = models.ForeignKey(Member, on_delete=models.DO_NOTHING)
+    cname = models.ForeignKey(Shop, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'possession'     # 생성할 테이블 이름 지정
+        ordering = ['id']
+
+    def __str__(self):
+        return self.userid     # 객체 출력시 출력형태 정의
--- pay/views.py
+++ pay/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from member.models import Member
+from pay.models import Shop
 
 
 def pay(request):
-    return render(request, 'pay.html')
+    if request.method == 'GET':
+        bds = Shop.objects.all()
+        # if request.session.get('userid'):
+        #     if
+        context = {'bds': bds}
+        return render(request, 'pay.html', context)
+    elif request.method == 'POST':
+        error=''
+        form = request.POST.dict()
+        print(form)
 
+        m = Member.objects.all()
+        if m['name']==form['name']:
+            print('ss')
+
+        return render(request, 'payok.html')
 
 def payok(request):
     return render(request, 'payok.html')
