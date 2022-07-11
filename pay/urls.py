8bc4546fdc75223cd0972fa768eaf7bd796867aa
LeeByeongHwi <diplopia7@gmail.com>
2022-07-11 15:08:25 +0900

    이병휘 - possession 연동 및 페이지 전환 완료

--- pay/urls.py
+++ pay/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('pay/', views.pay, name='pay'),
     path('payok/', views.payok, name='payok'),
     path('buy/', views.buy, name='buy'),
-    path('userpos/',views.userpos,name='userpos')
+    path('userpos/',views.userpos,name='userpos'),
 ]
